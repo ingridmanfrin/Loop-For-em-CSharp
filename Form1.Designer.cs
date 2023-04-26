04fefcf814e4773fc3073c9eaad6c8fc504f4482
ingridmanfrin <ingrid.caldeira@ITLNB109>
2023-04-26 02:56:20 -0300

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Loop_For_em_CSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnOk = new System.Windows.Forms.Button();
+            this.ListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnOk
+            // 
+            this.BtnOk.Location = new System.Drawing.Point(163, 318);
+            this.BtnOk.Name = "BtnOk";
+            this.BtnOk.Size = new System.Drawing.Size(75, 23);
+            this.BtnOk.TabIndex = 0;
+            this.BtnOk.Text = "OK";
+            this.BtnOk.UseVisualStyleBackColor = true;
+            this.BtnOk.Click += new System.EventHandler(this.BtnOk_Click);
+            // 
+            // ListBox
+            // 
+            this.ListBox.FormattingEnabled = true;
+            this.ListBox.Location = new System.Drawing.Point(104, 67);
+            this.ListBox.Name = "ListBox";
+            this.ListBox.Size = new System.Drawing.Size(198, 225);
+            this.ListBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(454, 409);
+            this.Controls.Add(this.ListBox);
+            this.Controls.Add(this.BtnOk);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnOk;
+        private System.Windows.Forms.ListBox ListBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Loop_For_em_CSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void BtnOk_Click(object sender, EventArgs e)
+        {
+            //ListBox.Items.Add(1);
+            //ListBox.Items.Add(2);
+            //ListBox.Items.Add(3);
+            //ListBox.Items.Add(50);
+
+            //For
+            for (int valor = 0; valor <= 50; valor++) //++ incremento
+            {
+                ListBox.Items.Add(valor);
+            }
+
+            ////For para apresentar números ímpares
+            //for (int valor = 1; valor <= 30; valor += 2) //++ incremento
+            //{
+            //    ListBox.Items.Add(valor);
+            //}
+
+            ////For para apresentar números decrescentes
+            //for (int valor = 200; valor >= 100; valor--) //-- incremento
+            //{
+            //    ListBox.Items.Add(valor);
+            //}
+
+            //Se escrever for e der tab + tab cria automaticamente essa estrutura:
+            //for (int i = 0; i < length; i++)
+            //{
+
+            //}
+        }
+    }
+}
